522b782e26ddb03a927486d8ba18bb23d69515c8
Sina Nejati <snejati86@gmail.com>
2012-10-10 19:29:56 -0700

    Updated LinkedList

--- spec/LinkedListSpec.js
+++ spec/LinkedListSpec.js
@@ -11,89 +11,154 @@ describe("LinkedList", function() {
       expect(linkedList.first().value).toEqual(2);
     });
 
-//     it("returns null if there is no first node", function() {
-//       expect(linkedList.first()).toBeNull();
-//     });
+     it("returns null if there is no first node", function() {
+       expect(linkedList.first()).toBeNull();
+     });
   });
   
   describe(".last", function() {
-//     it("returns the value of the last node of the list", function() {
-//       linkedList.add(1,'pickles');
-//       linkedList.add(2,'herring');
-//       expect(linkedList.last().value).toEqual('herring');
-//     });
+     it("returns the value of the last node of the list", function() {
+       linkedList.add(1,'pickles');
+       linkedList.add(2,'herring');
+       expect(linkedList.last().value).toEqual('herring');
+     });
 
-//     it("returns null if there is no last node", function() {
-//       expect(linkedList.last()).toBeNull();
-//     });
+     it("returns null if there is no last node", function() {
+       expect(linkedList.last()).toBeNull();
+     });
   });
 
   describe(".add", function() {
-//     it("adds an node to the list", function() {
-//       expect(linkedList.length).toEqual(0);
-//       linkedList.add(2,'red');
-//       linkedList.add(3,'blue');
-//       linkedList.add("a string",'green');
-//       expect(linkedList.first().key).toEqual(2);
-//       expect(linkedList.last().key).toEqual("a string");
-//       expect(linkedList.length).toEqual(3);
-//     });
+     it("adds an node to the list", function() {
+       expect(linkedList.length).toEqual(0);
+       linkedList.add(2,'red');
+       linkedList.add(3,'blue');
+       linkedList.add("a string",'green');
+       expect(linkedList.first().key).toEqual(2);
+       expect(linkedList.last().key).toEqual("a string");
+       expect(linkedList.length).toEqual(3);
+     });
   });
 
   describe(".find", function() {
-//     it("returns null if the node is not in the list", function() {
-//       expect(linkedList.find(178)).toBeNull();
-//     });
+     it("returns null if the node is not in the list", function() {
+       expect(linkedList.find(178)).toBeNull();
+     });
 
-//     it("searches for a key and returns the value", function() {
-//       linkedList.add("small","mouse");
-//       linkedList.add("medium","rhino");
-//       linkedList.add("large","elephant");
-//       expect(linkedList.find("medium")).toBe("rhino");
-//     });
+     it("searches for a key and returns the value", function() {
+       linkedList.add("small","mouse");
+       linkedList.add("medium","rhino");
+       linkedList.add("large","elephant");
+       expect(linkedList.find("medium")).toBe("rhino");
+     });
   });
 
   describe(".remove", function() {
-//     it("removes the specified node from the list", function() {
-//       linkedList.add("red","fish");
-//       linkedList.add("blue","whale");
-//       linkedList.add("green","turtle");
+     it("removes the specified node from the list", function() {
+       linkedList.add("red","fish");
+       linkedList.add("blue","whale");
+       linkedList.add("green","turtle");
 
-//       linkedList.remove("blue");
+       linkedList.remove("blue");
 
-//       expect(linkedList.find("blue")).toBeNull();
-//       expect(linkedList.length).toEqual(2);
-//       expect(linkedList.find("green")).toEqual("turtle");
-//     });
+       expect(linkedList.find("blue")).toBeNull();
+       expect(linkedList.length).toEqual(2);
+       expect(linkedList.find("green")).toEqual("turtle");
+     });
 
-//     it("properly removes the first node", function() {
-//       linkedList.add(1,"fish");
-//       linkedList.add(2,"huge alligator");
-//       linkedList.remove(1);
-//       expect(linkedList.first().value).toEqual("huge alligator");
-//       expect(linkedList.length).toEqual(1);
-//     });
+     it("properly removes the first node", function() {
+       linkedList.add(1,"fish");
+       linkedList.add(2,"huge alligator");
+       linkedList.remove(1);
+       expect(linkedList.first().value).toEqual("huge alligator");
+       expect(linkedList.length).toEqual(1);
+     });
 
-//     it("properly removes the last node", function() {
-//       linkedList.add("first", "among equals");
-//       linkedList.add("last", "to be picked for sports");
-//       linkedList.remove("last");
-//       expect(linkedList.length).toEqual(1);
-//       expect(linkedList.last().key).toEqual("first");
-//       expect(linkedList.first()).toEqual(linkedList.last());
-//     });
+     it("properly removes the last node", function() {
+       linkedList.add("first", "among equals");
+       linkedList.add("last", "to be picked for sports");
+       linkedList.remove("last");
+       expect(linkedList.length).toEqual(1);
+       expect(linkedList.last().key).toEqual("first");
+       expect(linkedList.first()).toEqual(linkedList.last());
+     });
 
-//     it("does nothing if the element cannot be found", function() {
-//       linkedList.add("red","fish");
-//       linkedList.add("blue","whale");
-//       linkedList.add("green","turtle");
+     it("does nothing if the element cannot be found", function() {
+       linkedList.add("red","fish");
+       linkedList.add("blue","whale");
+       linkedList.add("green","turtle");
 
-//       linkedList.remove("chartreuse");
+       linkedList.remove("chartreuse");
 
-//       expect(linkedList.find("red")).toEqual("fish");
-//       expect(linkedList.find("blue")).toEqual("whale");
-//       expect(linkedList.find("green")).toEqual("turtle");
-//       expect(linkedList.length).toEqual(3);
-//     });
+       expect(linkedList.find("red")).toEqual("fish");
+       expect(linkedList.find("blue")).toEqual("whale");
+       expect(linkedList.find("green")).toEqual("turtle");
+       expect(linkedList.length).toEqual(3);
+     });
   });
+
+    /* Advanced LinkedList Tests */
+
+  describe(".findmid", function () {
+      it("Return the value of the object in the middle of our list", function () {
+          expect(linkedList.length).toEqual(0);
+          linkedList.add(1, 'One');
+          linkedList.add(2, 'Two');
+          linkedList.add(3, 'Three');
+          linkedList.add(4, 'MID');
+          linkedList.add(5, 'Five');
+          linkedList.add(6, 'Six');
+          linkedList.add(7, 'Seven');
+          expect(linkedList.findmid()).toEqual("MID");
+      });
+  });
+
+  describe(".sort", function () {
+      it("Sorts the linked list by KEY values (Keys are numbers)", function () {
+          expect(linkedList.length).toEqual(0);
+          linkedList.add(4, 'MID');
+          linkedList.add(5, 'Five');
+          linkedList.add(3, 'Three');
+          linkedList.add(1, 'FIRST');
+          linkedList.add(7, 'LAST');
+          linkedList.add(2, 'Two');
+          linkedList.add(6, 'Six');
+          linkedList.sort();
+          expect(linkedList.first().value).toEqual("FIRST");
+          expect(linkedList.last().value).toEqual("LAST");
+      });
+  });
+
+  describe(".swap", function () {
+      it("Swaps two nodes in the linked list using KEY values", function () {
+          expect(linkedList.length).toEqual(0);
+          linkedList.add(4, 'MID');
+          linkedList.add(5, 'TO_BE_LAST');
+          linkedList.add(3, 'Three');
+          linkedList.add(1, 'FIRST');
+          linkedList.add(7, 'LAST');
+          linkedList.add(2, 'TO_BE_FIRST');
+          linkedList.add(6, 'Six');
+          linkedList.swap(5, 2);
+          linkedList.remove(4);
+          linkedList.remove(6);
+          expect(linkedList.first().value).toEqual("TO_BE_FIRST");
+          expect(linkedList.last().value).toEqual("TO_BE_LAST");
+      });
+  });
+
+  describe(".printReverse", function () {
+      it("Prints the list in Reverse order", function () {
+          expect(linkedList.length).toEqual(0);
+          linkedList.add(4, 'Four');
+          linkedList.add(5, 'Five');
+          linkedList.add(3, 'Three');
+          linkedList.add(1, 'First');
+          linkedList.add(7, 'Seven');
+          linkedList.add(2, 'Two');
+          linkedList.add(6, 'Six');
+          expect(linkedList.printReverse()).toEqual("Six,Two,Seven,First,Three,Five,Four");
+      });
+  });
+
 });
--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,15 +1,112 @@
-//LINKED LIST
+
+/* Constructors */ 
 function LinkedList() {
-  this.firstNode = null;
+    this.length = 0 ;
+    this.head = null;
+    this.tail = null;
 }
 
-LinkedList.prototype.add = function(key, value) {
-};
-
-
-//NODE
 function Node(key, value) {
-  this.key = key;
-  this.value = value;
-  this.nextNode = null;
+    this.key = key;
+    this.value = value;
+    this.nextNode = null;
 }
+
+/* LinkedList Member Functions */
+
+LinkedList.prototype.add = function (key, value) {
+    var newnode = new Node(key, value);
+    if (this.length < 1) {
+        this.head = newnode;
+        this.tail = newnode;
+    }
+    else {
+        this.tail.nextNode = newnode;
+        this.tail = newnode;
+    }
+    this.length++; 
+}
+
+LinkedList.prototype.remove = function (key) {
+    if (this.length < 1) {
+        return false;
+    }
+    else {
+        if (this.head.key == key) {
+            this.head = this.head.nextNode;
+            this.length--;
+            return true;
+        }
+        var prev = this.head;
+        var current = this.head.nextNode;
+        while (current != null) {
+            if (current.key == key) {
+                prev.nextNode = current.nextNode;
+                this.length--;
+                if (current.nexNode == null) {
+                    this.tail = prev;
+                }
+                return true;
+            }
+            current = current.nextNode;
+            prev = prev.nextNode;
+        }
+        return false;
+    }
+}
+
+LinkedList.prototype.removeLast = function () {
+    if (this.length < 1) {
+        return;
+    }
+    else {
+        var prev = this.head;
+        var current = this.head.nextNode;
+        while (current.value != null) {
+            current = current.nextNode;
+            prev = prev.nextNode;
+        }
+        this.tail = prev;
+        this.length--;
+    }
+}
+        
+LinkedList.prototype.removeFirst = function () {
+        if (this.length < 1) {
+            return;
+        }
+        else {
+            this.head = this.head.nextNode;
+        }
+        this.length--;
+    }
+
+LinkedList.prototype.first = function () {
+        return this.head;
+}
+
+LinkedList.prototype.last = function () {
+    return this.tail;
+}
+
+ LinkedList.prototype.find = function (key) {
+        if (this.length < 1) {
+            return null;
+        }
+        else
+        {
+            var current = this.head;
+            while(current!=null)
+            {
+                if(current.key==key)
+                {
+                    return current.value;
+                }
+                current=current.nextNode;
+            }
+            return null;
+        }
+    }
+
+
+
